0580c02cc33ffd985aa3f18e26bcc2b8f84175d0
Lisa Jung <lisahjung@gmail.com>
2020-03-23 15:41:24 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,61 +1,3 @@
-def using_include (array, element)
-  array.include? element
-end
-
-def using_sort (array)
-  array.sort 
-end
-
-def using_reverse(array)
-  array.reverse
-end
-
-def using_first(array)
-  array.first
-end
-
-def using_last(array)
-  array.last
-end
-
-def using_size(array)
-  array.size
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-=begin
 def using_include(array, element)
   array.include?(element)
 end
@@ -79,4 +21,3 @@ end
 def using_size(array)
 array.size
 end
-=end
